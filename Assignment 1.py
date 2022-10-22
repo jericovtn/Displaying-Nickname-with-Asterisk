765c25e404c9e6c7a3b0b5af4b0356534cef860b
Jerico James Viteño <93725569+jericovtn@users.noreply.github.com>
2022-10-22 18:35:34 +0800

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,9 @@
+# Name: Jerico James F. Viteño
+# Assignment 1: Nickname
+# October 22, 2022
+
+print("******   *****    ****")
+print("*       *        *    *")
+print("****    *        *    *")
+print("*       *        *    *")
+print("******   *****    ****")
